880d7c8bb15832c433bafdd3c8825d4ef246aaa3
Valerie Hernández Fernández <valerie.herfer@gmail.com>
2022-04-03 16:02:21 -0600

    UI Components Optimized

--- src/SweetSwarm.java
+++ src/SweetSwarm.java
@@ -1,16 +1,26 @@
 import java.awt.*;
+import java.awt.event.*;
 import java.io.*;
 import javax.swing.*;
 
 /**
- *
  * @author <a href="https://github.com/valeriehernandez-7">Valerie M. Hernández Fernández</a>
  */
-public class SweetSwarm extends JFrame {
+public class SweetSwarm extends JFrame implements ActionListener {
     // ui components
     private JButton playBtn, pauseBtn, slowerBtn, fasterBtn;
-    private JLabel scoreLbl, windowBackgroundLbl;
+    private JLabel scoreLbl, backgroundLbl;
     private Font fontSource, font;
+    // resources
+    private final ImageIcon backgroundImg = new ImageIcon("src/resources/img/__background.png");
+    private final ImageIcon playBtn0Img = new ImageIcon("src/resources/img/__btn-play-0.png");
+    private final ImageIcon playBtn1Img = new ImageIcon("src/resources/img/__btn-play-1.png");
+    private final ImageIcon pauseBtn0Img = new ImageIcon("src/resources/img/__btn-pause-0.png");
+    private final ImageIcon pauseBtn1Img = new ImageIcon("src/resources/img/__btn-pause-1.png");
+    private final ImageIcon slowerBtn0Img = new ImageIcon("src/resources/img/__btn-slower-0.png");
+    private final ImageIcon slowerBtn1Img = new ImageIcon("src/resources/img/__btn-slower-1.png");
+    private final ImageIcon fasterBtn0Img = new ImageIcon("src/resources/img/__btn-faster-0.png");
+    private final ImageIcon fasterBtn1Img = new ImageIcon("src/resources/img/__btn-faster-1.png");
     // game features
     private boolean gameOver = false;
     private boolean gamePaused = false;
@@ -19,6 +29,7 @@ public class SweetSwarm extends JFrame {
 
     /**
      * SweetSwarm class constructor.
+     *
      * @author <a href="https://github.com/valeriehernandez-7">Valerie M. Hernández Fernández</a>
      */
     public SweetSwarm() {
@@ -35,6 +46,7 @@ public class SweetSwarm extends JFrame {
 
     /**
      * Instantiate and customize the interface components.
+     *
      * @author <a href="https://github.com/valeriehernandez-7">Valerie M. Hernández Fernández</a>
      */
     private void getUIComponents() {
@@ -44,82 +56,18 @@ public class SweetSwarm extends JFrame {
             font = fontSource.deriveFont(Font.PLAIN, 24);
             // --- buttons ---
             // play button
-            playBtn = new JButton();
-            playBtn.setIcon(new ImageIcon("src/resources/img/__btn-play-1.png"));
-            playBtn.setDisabledIcon(new ImageIcon("src/resources/img/__btn-play-0.png"));
-            playBtn.setBounds(187, 333, playBtn.getIcon().getIconWidth(), playBtn.getIcon().getIconHeight());
-            playBtn.setBorder(BorderFactory.createEmptyBorder());
-            playBtn.setContentAreaFilled(false);
-            playBtn.setVisible(true);
-            playBtn.addActionListener(Event -> {
-                System.out.println("⬢   ▶️PLAY");
-                gamePaused = false;
-                playBtn.setVisible(false);
-                playBtn.setEnabled(false);
-                pauseBtn.setVisible(true);
-                pauseBtn.setEnabled(true);
-                fasterBtn.setEnabled(score != 250);
-                slowerBtn.setEnabled(score != 2000);
-            });
+            playBtn = buttonSetup(playBtn1Img, playBtn0Img, 187, 333, true, true);
             getContentPane().add(playBtn);
             // pause button
-            pauseBtn = new JButton();
-            pauseBtn.setIcon(new ImageIcon("src/resources/img/__btn-pause-1.png"));
-            pauseBtn.setDisabledIcon(new ImageIcon("src/resources/img/__btn-pause-0.png"));
-            pauseBtn.setBounds(187, 333, pauseBtn.getIcon().getIconWidth(), pauseBtn.getIcon().getIconHeight());
-            pauseBtn.setBorder(BorderFactory.createEmptyBorder());
-            pauseBtn.setContentAreaFilled(false);
-            pauseBtn.setVisible(false);
-            pauseBtn.addActionListener(Event -> {
-                System.out.println("⬢   ⏸️PAUSE");
-                gamePaused = true;
-                playBtn.setVisible(true);
-                playBtn.setEnabled(true);
-                pauseBtn.setVisible(false);
-                pauseBtn.setEnabled(false);
-                slowerBtn.setEnabled(false);
-                fasterBtn.setEnabled(false);
-            });
+            pauseBtn = buttonSetup(pauseBtn1Img, pauseBtn0Img, 187, 333, true, false);
             getContentPane().add(pauseBtn);
             // slower button
-            slowerBtn = new JButton();
-            slowerBtn.setIcon(new ImageIcon("src/resources/img/__btn-slower-1.png"));
-            slowerBtn.setDisabledIcon(new ImageIcon("src/resources/img/__btn-slower-0.png"));
-            slowerBtn.setBounds(136, 333, slowerBtn.getIcon().getIconWidth(), slowerBtn.getIcon().getIconHeight());
-            slowerBtn.setBorder(BorderFactory.createEmptyBorder());
-            slowerBtn.setContentAreaFilled(false);
-            slowerBtn.setEnabled(false);
-            slowerBtn.setVisible(true);
-            slowerBtn.addActionListener(Event -> {
-                if (speed < 2000) {
-                    speed += 250;
-                    System.out.println("⬢   ⏪️SLOWER ⏰ " + speed + " ms");
-                    if (speed == 2000) {
-                        slowerBtn.setEnabled(false);
-                    }
-                }
-                fasterBtn.setEnabled(true);
-            });
+            slowerBtn = buttonSetup(slowerBtn1Img, slowerBtn0Img, 136, 333, false, true);
             getContentPane().add(slowerBtn);
             // faster button
-            fasterBtn = new JButton();
-            fasterBtn.setIcon(new ImageIcon("src/resources/img/__btn-faster-1.png"));
-            fasterBtn.setDisabledIcon(new ImageIcon("src/resources/img/__btn-faster-0.png"));
+            fasterBtn = buttonSetup(fasterBtn1Img, fasterBtn0Img, 247, 333, false, true);
+            ;
             fasterBtn.setBounds(247, 333, fasterBtn.getIcon().getIconWidth(), fasterBtn.getIcon().getIconHeight());
-            fasterBtn.setBorder(BorderFactory.createEmptyBorder());
-            fasterBtn.setContentAreaFilled(false);
-            fasterBtn.setEnabled(false);
-            fasterBtn.setVisible(true);
-            fasterBtn.addActionListener(Event -> {
-                if (250 < speed) {
-                    speed -= 250;
-                    System.out.println("⬢   ⏩️FASTER ⏰ " + speed + " ms");
-                    if (speed == 250) {
-                        fasterBtn.setEnabled(false);
-                    }
-                }
-                slowerBtn.setEnabled(true);
-            });
             getContentPane().add(fasterBtn);
             // --- labels ---
             // score label
@@ -132,17 +80,74 @@ public class SweetSwarm extends JFrame {
             scoreLbl.setText(String.valueOf(score));
             getContentPane().add(scoreLbl);
             // background label
-            windowBackgroundLbl = new JLabel(new ImageIcon("src/resources/img/__background.png"));
-            windowBackgroundLbl.setBounds(0, 0, windowBackgroundLbl.getIcon().getIconWidth(), windowBackgroundLbl.getIcon().getIconHeight());
-            getContentPane().add(windowBackgroundLbl);
+            backgroundLbl = labelSetup(backgroundImg, 0, 0, true);
+            getContentPane().add(backgroundLbl);
         } catch (IOException | FontFormatException e) {
             e.printStackTrace();
         }
     }
 
-    /**
-     * Instantiate and spawn the bees.
-     * @author <a href="https://github.com/Mariana612">Mariana Navarro Jiménez</a>
-     */
-    protected void beeGenerator(){}
+    private JButton buttonSetup(ImageIcon source, ImageIcon effect, int posX, int posY, boolean enabled, boolean visible) {
+        JButton button = new JButton();
+        button.setIcon(source);
+        button.setDisabledIcon(effect);
+        button.setPressedIcon(effect);
+        button.setRolloverIcon(effect);
+        button.setBounds(posX, posY, button.getIcon().getIconWidth(), button.getIcon().getIconHeight());
+        button.setBorder(BorderFactory.createEmptyBorder());
+        button.setContentAreaFilled(false);
+        button.setEnabled(enabled);
+        button.setVisible(visible);
+        button.addActionListener(this);
+        return button;
+    }
+
+    private JLabel labelSetup(ImageIcon source, int posX, int posY, boolean visible) {
+        JLabel label = new JLabel(source);
+        label.setBounds(posX, posY, label.getIcon().getIconWidth(), label.getIcon().getIconHeight());
+        label.setVisible(visible);
+        return label;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if (source == playBtn) {
+            System.out.println("⬢   ▶️PLAY");
+            gamePaused = false;
+            playBtn.setVisible(false);
+            playBtn.setEnabled(false);
+            pauseBtn.setVisible(true);
+            pauseBtn.setEnabled(true);
+            fasterBtn.setEnabled(score != 250);
+            slowerBtn.setEnabled(score != 2000);
+        } else if (source == pauseBtn) {
+            System.out.println("⬢   ⏸️PAUSE");
+            gamePaused = true;
+            playBtn.setVisible(true);
+            playBtn.setEnabled(true);
+            pauseBtn.setVisible(false);
+            pauseBtn.setEnabled(false);
+            slowerBtn.setEnabled(false);
+            fasterBtn.setEnabled(false);
+        } else if (source == slowerBtn) {
+            if (speed < 2000) {
+                speed += 250;
+                System.out.println("⬢   ⏪️SLOWER ⏰ " + speed + " ms");
+                if (speed == 2000) {
+                    slowerBtn.setEnabled(false);
+                }
+            }
+            fasterBtn.setEnabled(true);
+        } else if (source == fasterBtn) {
+            if (250 < speed) {
+                speed -= 250;
+                System.out.println("⬢   ⏩️FASTER ⏰ " + speed + " ms");
+                if (speed == 250) {
+                    fasterBtn.setEnabled(false);
+                }
+            }
+            slowerBtn.setEnabled(true);
+        }
+    }
 }
