c04508f71ec4039276185a7a48ff22d20665546f
Mariana12 <mariananavarro3022@gmail.com>
2022-04-24 22:42:19 -0600

    Collector new Attackrespone

--- src/game/bee/Collector.java
+++ src/game/bee/Collector.java
@@ -1,6 +1,9 @@
 package game.bee;
 
 import game.SweetSwarm;
+import game.object.Threat;
+
+import java.awt.*;
 
 /**
  * @author <a href="https://github.com/Mariana612">Mariana Navarro Jiménez</a>
@@ -16,5 +19,18 @@ public class Collector extends Bee {
     }
 
     @Override
-    public void attackResponse(SweetSwarm sweetSwarm) {}
+    public void attackResponse(SweetSwarm sweetSwarm) {
+        for(Threat threat: sweetSwarm.threats){
+            if (this.getCell() == threat.getCell()) {
+                this.setHealth(this.getHealth() - threat.getPower());
+                Point[] neighborsList = sweetSwarm.honeycomb.getNeighbors(new Point(this.getCell()[0],this.getCell()[1]));
+                for(Point neighbor: neighborsList){
+                    if(sweetSwarm.honeycomb.getMap()[neighbor.x][neighbor.y].isAvailable()){
+                        this.setLocation(sweetSwarm.honeycomb.getMap()[neighbor.x][neighbor.y].getX(),sweetSwarm.honeycomb.getMap()[neighbor.x][neighbor.y].getY());
+                    }
+                    break;
+                }
+            }
+        }
+    }
 }
